69be7828679b5eb6d4ee580bb5807f26b703ddb1
Paul Badu Yakubu <125946856+PaulBaduYakubu@users.noreply.github.com>
2024-06-20 09:25:33 -0400

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
 # PROGRAMMER: Paul Badu Yakubu
 # DATE CREATED: 05/19/2024                                  
